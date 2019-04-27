c325469bf1b8676a69f9cebf9a4005e47ff2152e
Papp Mihály <papp.misi@protonmail.com>
2019-04-27 19:43:48 +0200

    alap kárgya layout

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  selector: "app-tab1",
+  templateUrl: "tab1.page.html",
+  styleUrls: ["tab1.page.scss"]
 })
-export class Tab1Page {}
+export class Tab1Page {
+  private cim: String = "Vakáció a kastélyban";
+  private leiras: String = "Egy hetet pihentünk a Zürichsee-nél";
+  private helyszin: String = "Zürich, Svájc";
+  private kep: String = "sklafjléskdfjslkdfj";
+  private felhasznalo: String = "Misi";
+}
